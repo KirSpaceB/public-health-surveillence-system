979a03b888fb6ba2047eea17014ba825430e2d6f
KirSpaceB <productivekirk@gmail.com>
2024-08-22 13:23:40 -0700

    feat: added logic to correct empty geolocated fields

--- src/tooling/GeocodeData.ts
+++ src/tooling/GeocodeData.ts
@@ -1,4 +1,5 @@
 import { addressToLocations, locationToAddress, } from "@arcgis/core/rest/locator";
+import getStateAbbreviation from "./GetStateAbbreviations";
 // the issue here is that we might need to format it in "County, State" form
 async function geocodeCounty(county?:string, state?:string):Array {
   const url = "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer";
@@ -16,7 +17,7 @@ async function geocodeCounty(county?:string, state?:string):Array {
               "SingleLine": county
             },
             // changing max locatio doesn't do anything from my experience
-            maxLocations: 5
+            maxLocations: 25
           },
       );
       console.log("results from geocodeCounty", results);
@@ -28,8 +29,29 @@ async function geocodeCounty(county?:string, state?:string):Array {
         geocodedLocationCovertedToCoordinates.push(x, y);
       }
     } catch(error) {
-      console.log(error as Error['message'])
+      console.log("This is the county it failed to fetch", county);
+      console.log("This is the error message from catch block", error as Error['message']);
+      try {
+        const stateAbbreviation = getStateAbbreviation(state);
+        const resultsUsingCountyAndState = await addressToLocations(url,
+          {
+            address: {
+              "City": county,
+              "State": stateAbbreviation
+            }
+          }
+        )
+        console.log("resultsUsingCountyAndState", resultsUsingCountyAndState)
+        if(resultsUsingCountyAndState.length > 0) {
+          const {x,y} = resultsUsingCountyAndState[0].location;
+          geocodedLocationCovertedToCoordinates.push(x,y)
+        }
+      } catch(error) {
+        console.log("Something went wrong")
+      }
     }
+  } else {
+    console.log("This county is undefined", county);
   }
   return geocodedLocationCovertedToCoordinates;
 }
---
+++ src/tooling/GetStateAbbreviations.ts
@@ -0,0 +1,61 @@
+const stateAbbreviations:{[key:string]: string} = {
+  "Alabama": "AL",
+  "Alaska": "AK",
+  "Arizona": "AZ",
+  "Arkansas": "AR",
+  "California": "CA",
+  "Colorado": "CO",
+  "Connecticut": "CT",
+  "Delaware": "DE",
+  "Florida": "FL",
+  "Georgia": "GA",
+  "Hawaii": "HI",
+  "Idaho": "ID",
+  "Illinois": "IL",
+  "Indiana": "IN",
+  "Iowa": "IA",
+  "Kansas": "KS",
+  "Kentucky": "KY",
+  "Louisiana": "LA",
+  "Maine": "ME",
+  "Maryland": "MD",
+  "Massachusetts": "MA",
+  "Michigan": "MI",
+  "Minnesota": "MN",
+  "Mississippi": "MS",
+  "Missouri": "MO",
+  "Montana": "MT",
+  "Nebraska": "NE",
+  "Nevada": "NV",
+  "New Hampshire": "NH",
+  "New Jersey": "NJ",
+  "New Mexico": "NM",
+  "New York": "NY",
+  "North Carolina": "NC",
+  "North Dakota": "ND",
+  "Ohio": "OH",
+  "Oklahoma": "OK",
+  "Oregon": "OR",
+  "Pennsylvania": "PA",
+  "Rhode Island": "RI",
+  "South Carolina": "SC",
+  "South Dakota": "SD",
+  "Tennessee": "TN",
+  "Texas": "TX",
+  "Utah": "UT",
+  "Vermont": "VT",
+  "Virginia": "VA",
+  "Washington": "WA",
+  "West Virginia": "WV",
+  "Wisconsin": "WI",
+  "Wyoming": "WY"
+}
+
+// Function to get the abbreviation for a given state name
+export default function getStateAbbreviation(stateName: string): string {
+  if(stateName) {
+    return stateAbbreviations[stateName];
+  } else {
+    console.log("Statename was undefined", stateName);
+  }
+}
