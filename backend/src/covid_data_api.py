c8d733dc6bb31bc618938f3df3082e9535be04ec
Kirk Flores <113055345+KirSpaceB@users.noreply.github.com>
2024-08-26 13:49:22 -0700

    feat: connected python backend to mysql database

--- backend/src/covid_data_api.py
+++ backend/src/covid_data_api.py
@@ -1,22 +1,16 @@
-import pymysql
+from config import DB_HOST, DB_USERNAME, DB_PASSWORD, DB_PORT, DB_DATABASE
 from sqlalchemy import create_engine
 import pandas as pd;
 
-# Connection details
-host_ip = '10.255.255.254'
-username = 'root'
-password = 'Spooderman1$'  # replace with your MySQL root password
-port = '3306'
-database = 'public_health_data'  # replace with your database name
-
 # Create a connection string
-connection_string = f"mysql+pymysql://{username}:{password}@{host_ip}:{port}/{database}"
+connection_string = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}"
 
 # Create an engine
 engine = create_engine(connection_string)
 
 # Test the connection by fetching some data
-query = "SELECT * FROM united_states_covid-19_community_levels_by_county LIMIT 5"  # replace with your table name
+query = "SELECT * FROM `united_states_covid-19_community_levels_by_county` LIMIT 5;"
+
 df = pd.read_sql(query, engine)
 
 print(df)
